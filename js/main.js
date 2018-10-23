f0b0220303e5c0180271546c1d2d93646cf94d20
AlberJ <alberjonathas@gmail.com>
2018-10-23 10:22:20 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var categoria_arvore = {
+    img: document.createElement("226663-ecology/226663-ecology/png/tree.png"),
+    selecionado: null,
+    titulo:"Árvore",
+    desc:"texto mimimi bla bla bla"
+};
+var categoria_lixo = {
+    img: document.createElement("226663-ecology/226663-ecology/png/tree.png"),
+    selecionado: null,
+    titulo:"Lixo",
+    desc:"texto mimimi bla bla bla"
+};
+var categoria_exemplo = {
+    img: document.createElement("226663-ecology/226663-ecology/png/tree.png"),
+    selecionado: null,
+    titulo:"Exemplo",
+    desc:"texto mimimi bla bla bla"
+};
+
+function carrega_array(){
+    array = {}
+}
